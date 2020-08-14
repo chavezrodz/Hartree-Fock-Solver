025119b53b5962477fd02d1172ea3bca11e75e1e
Rodrigo <chavezrodz@gmail.com>
2020-08-14 17:55:47 -0400

    update

---
+++ Code/PhaseDiagramSweep.py
@@ -0,0 +1,121 @@
+import numpy as np
+import itertools
+import sys
+from numpy import linalg as LA
+from multiprocessing import Process
+from itertools import product
+from multiprocessing import Pool
+from time import time
+
+from Hamiltonian_8 import *
+# from Code.PhaseDiagramPoint import *
+from Code.HFA_Solver import *
+"""
+Need to Check:
+print out results in csv ?
+"""
+
+n_threads = 8
+
+# if  len(sys.argv)!=2:
+    # print("Expected input")
+    # exit(2)
+
+# n = int(sys.argv[1])
+n = 4
+# model parameters
+Model_Params = dict(
+N_Dim = 2,
+N_cells = 10,
+Filling = 0.25,
+mat_dim = 8,
+
+eps = 0,
+t_1 = 1,
+U = 1,
+J = 1)
+
+##################### Declare MFP Range ################
+
+Delta_range = np.arange(2,10,1)
+SFM_range = np.arange(2,10,1)
+SAFM_range = np.arange(2,10,1)
+
+MFP_itterator = product(Delta_range,SFM_range,SAFM_range)
+
+params_list = []
+for i in MFP_itterator:
+	params_list.append(i)
+
+####################### Calling MF_params ###############
+
+MF_params = np.array(params_list[n])
+
+################## Declare Phase Diagram Grid size ##################
+
+U_range = 10
+J_range = 10
+
+############# Create Energies empty grid
+Es_trial = np.zeros((U_range,J_range))
+
+########## Create Phase Diagram Itterator
+U_range = np.arange(U_range)
+J_range = np.arange(J_range)
+
+Iterator = product(U_range,J_range)
+
+
+def Phase_Diagram_point(v):
+	Model_Params['U'],Model_Params['J'] = v[0]/10,v[1]/10
+
+	Model = Hamiltonian(Model_Params, MF_params)
+	
+	Sol = HFA_Solver(Model)
+	
+	Sol.Itterate(tol=1e-3,verbose=False)
+	
+	Model.E_occ = Sol.total_occupied_energy
+
+	Model.Calculate_Energy()
+	return Model.Final_Total_Energy
+
+
+
+# Energies results to list
+
+name = str(MF_params)
+
+a = time()
+
+with Pool(n_threads) as p:
+	results = p.map(Phase_Diagram_point, Iterator)
+
+print('Time to complete model parameters sweep:',round(time() - a,ndigits=3), 'seconds')
+
+# Energies results list to array to csv
+Iterator = product(U_range,J_range)
+for i,v in enumerate(Iterator):
+	Es_trial[v] = results[i]
+
+np.savetxt('Results/Guesses_Results/Guess'+name+'.csv',Es_trial,delimiter=',')
+
+##################### End of Phase Diagram Sweep ######################
+
+
+'''
+from Hamiltonian_2 import *
+from HFA_Solver import *
+Model_Params = dict(
+N_Dim = 2,
+N_cells = 50,
+Filling = 0.5,
+mat_dim = 2,
+eps = 1,
+t = 0.1,
+k_spring = 1,
+)
+
+# MFP guesses
+MF_params = np.array([1,1])
+'''
